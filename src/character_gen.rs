5e2c314bc46c041789196fdac460e7748b681539
a.yamaev <a.yamaev@smartengines.com>
2021-12-04 14:53:17 +0300

    New question

--- src/character_gen.rs
+++ src/character_gen.rs
@@ -14,6 +14,33 @@ pub struct SimpleQuestion {
     answers : Vec<SimpleQuestionAnswer>
 }
 
+impl SimpleQuestion {
+    pub fn clone(&self) -> Self {
+        let head = String::from(self.question.as_str());
+        let mut answers = Vec::<SimpleQuestionAnswer>::new();
+        for a in &self.answers {
+            match a {
+                SimpleQuestionAnswer::NewQuestion(s, q) => {
+                    answers.push(SimpleQuestionAnswer::NewQuestion(
+                        String::from(s.as_str()),
+                        Box::new(SimpleQuestion::clone(q)))
+                    );
+                }
+                SimpleQuestionAnswer::Result(s) => {
+                    answers.push(SimpleQuestionAnswer::Result(
+                        String::from(s.as_str())
+                    ));
+                }
+            }
+        }
+
+        Self { 
+            question : head,
+            answers
+        }
+    }
+}
+
 pub struct GenerationTypeScreen {
 
 }
@@ -43,13 +70,13 @@ impl NoviceGenScreen {
         root.question = String::from("Каков стиль боя вашего персонажа?");
 
         root.answers.push(SimpleQuestionAnswer::NewQuestion(
-            String::from("Буду сражаться с врагом лицом к лицу"),
+            String::from("Я не побоюсь выйти на бой лицом к лицу"),
             Box::new(NoviceGenScreen::hide_question())
         ));
 
         root.answers.push(SimpleQuestionAnswer::NewQuestion(
             String::from("Буду держаться на расстоянии, атакуя издалека"),
-            Box::new(NoviceGenScreen::hide_question())
+            Box::new(NoviceGenScreen::distan_question())
         ));
 
         root.answers.push(SimpleQuestionAnswer::NewQuestion(
@@ -60,27 +87,76 @@ impl NoviceGenScreen {
         root
     }
 
+    fn magick_question() -> SimpleQuestion {
+        let mut root = SimpleQuestion::new();
+
+        root.question = String::from("Ваш персонаж черпает магическую силу из?");
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("Глубоких знаний, полученных в академии"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("От темной сущности, что даровала мне силы"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("От природных сил разлитых вокруг"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("Глубоких знаний, полученных в магической консерватории"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+        root
+    }
+
+    fn distan_question() -> SimpleQuestion {
+        let mut root = SimpleQuestion::new();
+        root.question = String::from("Мой персонаж больше тягеет к ?");
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("Использованию божественных сил"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("Проявлению магического искуства"),
+            Box::new(NoviceGenScreen::magick_question())
+        ));
+
+        root.answers.push(SimpleQuestionAnswer::NewQuestion(
+            String::from("Честной воинской стали"),
+            Box::new(NoviceGenScreen::hide_question())
+        ));
+
+        root
+    }
+
     fn hide_question() -> SimpleQuestion {
         let mut root = SimpleQuestion::new();
         root.question = String::from("Закон никогда не был препятствием вашему персонажу?");
+
+        root.answers.push(SimpleQuestionAnswer::Result(
+            String::from("Несомненно")
+        ));
+        root.answers.push(SimpleQuestionAnswer::Result(
+            String::from("Мой персонаж предпочитает сохранять справедливость в мире")
+        ));
 
         root
     }
 }
 
+
+
 impl Screen for NoviceGenScreen {
     fn update(&mut self, ctx: &CtxRef, frame: &mut Frame<'_>) -> ScreenResult {
-
-        // let mut f = egui::FontDefinitions::default();
-        // f.family_and_size.insert(
-        //     TextStyle::Button,
-        //     (FontFamily::Proportional, 32.0)
-        // );
-        // f.family_and_size.insert(
-        //     TextStyle::Heading,
-        //     (FontFamily::Proportional, 48.0)
-        // );
-        // ctx.set_fonts(f);
 
         egui::CentralPanel::default().show(ctx, |ui| {
 
@@ -88,16 +164,31 @@ impl Screen for NoviceGenScreen {
            ui.add(egui::Label::new(self.question.question.as_str()).text_style(TextStyle::Heading));
             ui.separator();
 
+            let mut new_question = Option::<SimpleQuestion>::None;
+
             for a in &self.question.answers {
                 match a {
                     SimpleQuestionAnswer::NewQuestion(s, q) => {
                         if ui.button(s.as_str()).clicked() {
-
+                            new_question = Option::Some(SimpleQuestion::clone(q));
                         }
+                        ui.separator();
                     }
                     SimpleQuestionAnswer::Result(r) => {
+                        if ui.button(r.as_str()).clicked() {
 
+                        }
+                        ui.separator();
                     }
+                }
+            }
+
+            match new_question {
+                Option::Some(q) => {
+                    self.question = q;
+                }
+                Option::None => {
+
                 }
             }
         });
